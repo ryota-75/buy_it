9d615de65c139be04e5a628350c2184899e52ac1
EC2 Default User <ec2-user@ip-172-31-42-24.ap-northeast-1.compute.internal>
2020-06-08 14:03:58 +0000

    itemモデルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'items#index'
+    resources :tweets do
+    resources :comments, only: [:create]
+  end
+  resources :users, only: [:show]
 end
