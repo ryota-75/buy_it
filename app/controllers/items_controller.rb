68e64a28f37ddbd5993cc5f1e071b2db8cc45415
EC2 Default User <ec2-user@ip-172-31-42-24.ap-northeast-1.compute.internal>
2020-06-15 13:09:17 +0000

    画像アップロードを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,30 @@
 class ItemsController < ApplicationController
   
+  before_action :move_to_index, except: :index
+  
   def index
+    @items = Item.all.order("created_at DESC").page(params[:page]).per(9)
   end
+  
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    # Item.create(item_name: item_params[:item_name], price: item_params[:price],store: item_params[:store], image: item_params[:image], text: item_params[:text], user_id:current_user.id)
+    Item.create(image: item_params[:image],user_id:current_user.id)
+    redirect_to :root
+  end
+  
+  
+  private
+  def item_params
+    # params.permit(:item_name, :price, :store, :text)
+    params.require(:item).permit(:content, :image)
+  end
+  
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
 end
